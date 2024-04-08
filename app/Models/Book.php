8b7fa47ec25b5528bef50815ef2741819bd53710
Mohammad Sulaiman <smensulaiman007@gmail.com>
2024-04-08 15:46:17 +0900

    IDE HELPER ADDED

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,9 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $author
+ * @property string $language
+ * @property string|null $published_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Review> $reviews
+ * @property-read int|null $reviews_count
+ * @method static \Database\Factories\BookFactory factory($count = null, $state = [])
+ * @method static Builder|Book highestRated()
+ * @method static Builder|Book newModelQuery()
+ * @method static Builder|Book newQuery()
+ * @method static Builder|Book popular()
+ * @method static Builder|Book query()
+ * @method static Builder|Book title(string $title)
+ * @method static Builder|Book whereAuthor($value)
+ * @method static Builder|Book whereCreatedAt($value)
+ * @method static Builder|Book whereId($value)
+ * @method static Builder|Book whereLanguage($value)
+ * @method static Builder|Book wherePublishedAt($value)
+ * @method static Builder|Book whereTitle($value)
+ * @method static Builder|Book whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Book extends Model
 {
     use HasFactory;
@@ -13,4 +43,22 @@ class Book extends Model
     {
         return $this->hasMany(Review::class);
     }
+
+    public function scopeTitle(Builder $queryBuilder, string $title): Builder
+    {
+        return $queryBuilder->where('title', 'LIKE', "%$title%");
+    }
+
+    public function scopePopular(Builder $queryBuilder): Builder
+    {
+        return $queryBuilder->withCount('reviews')
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $queryBuilder): Builder
+    {
+        return $queryBuilder->withAvg('reviews', 'rating')
+            ->orderBy('reviews_avg_rating','desc');
+    }
+
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -5,6 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $review
+ * @property int $rating
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $book_id
+ * @property-read \App\Models\Book $book
+ * @method static \Database\Factories\ReviewFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Review newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Review newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Review query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Review whereBookId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Review whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Review whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Review whereRating($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Review whereReview($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Review whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Review extends Model
 {
     use HasFactory;
